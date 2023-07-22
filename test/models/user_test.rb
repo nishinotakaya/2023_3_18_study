7e6c2473d119e37b499391c8116cd3e1dc2deba9
nishinotakaya <takaya314boxing@gmail.com>
2023-07-22 09:09:20 +0900

    fix/yml

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :bigint           not null, primary key
+#  email           :string(255)
 #  name            :string(255)      not null
 #  password_digest :string(255)      not null
 #  created_at      :datetime         not null
